ea32d06f8e63b55a8e5212b80d5613e1dea2cae0
Brendan Forster <me@brendanforster.com>
2013-02-03 22:59:25 +1100

    added project for test data

---
+++ src/PanoramaExample/MahApps.Metro.SampleData/Album.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace MahApps.Metro.SampleData
+{
+    public class Album
+    {
+        public int AlbumId { get; set; }
+
+        [DisplayName("Genre")]
+        public int GenreId { get; set; }
+
+        [DisplayName("Artist")]
+        public int ArtistId { get; set; }
+
+        public string Title { get; set; }
+
+        public decimal Price { get; set; }
+
+        [DisplayName("Album Art URL")]
+        public string AlbumArtUrl { get; set; }
+
+        public virtual Genre Genre { get; set; }
+        public virtual Artist Artist { get; set; }
+    }
+}
---
+++ src/PanoramaExample/MahApps.Metro.SampleData/Artist.cs
@@ -0,0 +1,8 @@
+﻿namespace MahApps.Metro.SampleData
+{
+    public class Artist
+    {
+        public int ArtistId { get; set; }
+        public string Name { get; set; }
+    }
+}
